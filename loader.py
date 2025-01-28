268034bc07def9023a666dc75897c3b389263065
Aleks Kulikov <59150713+kulikov-mister@users.noreply.github.com>
2025-01-28 20:43:18 +0500

    Update loader.py

--- loader.py
+++ loader.py
@@ -8,6 +8,5 @@ from config_data.config import Config, load_config
 
 config: Config = load_config()
 
-bot = Bot(token=config.tg_bot.token, default=DefaultBotProperties(
-    parse_mode="HTML"))
+bot = Bot(token=config.tg_bot.token, default=DefaultBotProperties(parse_mode="HTML"))
 dp = Dispatcher(storage=MemoryStorage(), fsm_strategy=FSMStrategy.GLOBAL_USER)
