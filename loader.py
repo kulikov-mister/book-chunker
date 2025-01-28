ab5b3eaa24aa836a8b8af28b5735621cb6f83652
Aleks Kulikov <59150713+kulikov-mister@users.noreply.github.com>
2025-01-28 20:43:01 +0500

    Update loader.py

--- loader.py
+++ loader.py
@@ -2,9 +2,12 @@
 from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.fsm.strategy import FSMStrategy
 from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
 from config_data.config import Config, load_config
+
 
 config: Config = load_config()
 
-bot = Bot(token=config.tg_bot.token, disable_web_page_preview=False, parse_mode="HTML", protect_content=True)
+bot = Bot(token=config.tg_bot.token, default=DefaultBotProperties(
+    parse_mode="HTML"))
 dp = Dispatcher(storage=MemoryStorage(), fsm_strategy=FSMStrategy.GLOBAL_USER)
